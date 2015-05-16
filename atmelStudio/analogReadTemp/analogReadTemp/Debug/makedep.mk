f4b526191b1869bb00e1541cb8e293a1070b9a56
Jesse Estum <estu0002@umn.edu>
2015-05-15 21:07:17 -0500

    Clean up of analogReadTemp

--- atmelStudio/analogReadTemp/analogReadTemp/Debug/makedep.mk
+++ atmelStudio/analogReadTemp/analogReadTemp/Debug/makedep.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-analogReadTemp.cpp
+..\..\..\..\..\..\..\arduino-1.6.3\hardware\arduino\avr\cores\arduino\main.cpp
 
-libs\main.cpp
+analogReadTemp.cpp
 
